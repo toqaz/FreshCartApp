99e6c80dc1e304bd3b575158cd8ac59b2a9c11bc
Toqa Youness <toqamohamedabdulaziz44@gmail.com>
2026-02-17 21:57:58 +0200

    router server

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'checkout/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'details/:slug/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
